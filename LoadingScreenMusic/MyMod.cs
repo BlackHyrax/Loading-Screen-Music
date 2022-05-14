f2aa8380a151fbbfe61a73fea4f69e82e2fac17f
⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ <JanTV720@gmail.com>
2022-05-14 19:50:35 +0200

    Added Functions
    Added Multi File Support and wav File Support.

--- LoadingScreenMusic/MyMod.cs
+++ LoadingScreenMusic/MyMod.cs
@@ -9,57 +9,116 @@ namespace LoadingScreenMusic
 {
     public class MyMod : MelonMod
     {
-        private AudioClip audiofile;
-        private AudioSource audiosource;
-        private AudioSource audiosource2;
-
         public override void OnApplicationStart()
         {
-            Directory.CreateDirectory("LoadingScreenMusic");
-            MelonCoroutines.Start(WebRequest());
+            if (!Directory.Exists("LoadingScreenMusic"))
+            {
+                Directory.CreateDirectory("LoadingScreenMusic");
+            }
         }
-        
-        IEnumerator WebRequest()
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            // I used some code of Knah´s join notifier for the unitywebrequest.
-
-            MelonLogger.Msg("Loading custom loading screen music");
-            var uwr = UnityWebRequest.Get($"file://{Path.Combine(Environment.CurrentDirectory, "LoadingScreenMusic/Music.ogg")}");
-            uwr.SendWebRequest();
-
-            while (!uwr.isDone) yield return null;
-
-            audiofile = WebRequestWWW.InternalCreateAudioClipUsingDH(uwr.downloadHandler, uwr.url, false, false, AudioType.UNKNOWN);
-            audiofile.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-
-            while (audiosource == null)
+            if (buildIndex == 1)
             {
-                audiosource = GameObject.Find("LoadingBackground_TealGradient_Music/LoadingSound")?.GetComponent<AudioSource>();
-
-                yield return null;
+                MelonCoroutines.Start(LoadingScreen());
             }
-            audiosource.clip = audiofile;
-            audiosource.Stop();
-            audiosource.Play();
-
-            while (audiosource2 == null)
+            if (buildIndex == 2)
             {
-                audiosource2 = GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/LoadingSound")?.GetComponent<AudioSource>();
-
-                yield return null;
+                MelonCoroutines.Start(ChangeLoadingScreen());
             }
-            audiosource2.clip = audiofile;
-            audiosource2.Stop();
-            audiosource2.Play();
-
-
         }
-
-
-
+        static System.Random random = new System.Random();
+        static string RandomMusicFile()
+        {
+            RandomMusicFile:
+            string[] musicfiles = Directory.GetFiles(Directory.GetCurrentDirectory() + "/LoadingScreenMusic");
+            bool filesdeleted = false;
+            foreach (string musicfile in musicfiles)
+            {
+                if (!musicfile.EndsWith(".wav") && !musicfile.EndsWith(".ogg"))
+                {
+                    MelonLogger.Warning($"Deleting {musicfile.Split('\\')[musicfile.Split('\\').Length-1]} because it isnt a Supported Audio Type (.wav or .ogg)");
+                    File.Delete(musicfile);
+                    filesdeleted = true;
+                }
+            }
+            if (filesdeleted)
+            {
+                goto RandomMusicFile;
+            }
+            if (musicfiles.Length == 0)
+            {
+                throw new Exception("No Audio file Found!");
+            }
+            if (musicfiles.Length == 1)
+            {
+                return musicfiles[0];
+            }
+            int index = random.Next(musicfiles.Length);
+            return musicfiles[index - 1];
+        }
+        IEnumerator ChangeLoadingScreen()
+        {
+            UnityWebRequest www = UnityWebRequest.Get("file://" + RandomMusicFile());
+            www.SendWebRequest();
+            while (!www.isDone)
+            {
+                yield return null;
+            }
+            AudioClip audioClip = WebRequestWWW.InternalCreateAudioClipUsingDH(www.downloadHandler, www.url, false, false, AudioType.UNKNOWN);
+            while (!www.isDone || audioClip.loadState == AudioDataLoadState.Loading)
+            {
+                yield return null;
+            }
+            if (audioClip != null)
+            {
+                if (loadingscreenAudio != null)
+                {
+                    loadingscreenAudio.clip = audioClip;
+                    loadingscreenAudio.Play();
+                }
+            }
+        }
+        static AudioSource loadingscreenAudio;
+        IEnumerator LoadingScreen()
+        {
+            GameObject authentication = GameObject.Find("LoadingBackground_TealGradient_Music/LoadingSound");
+            GameObject loadingscreen = GameObject.Find("MenuContent/Popups/LoadingPopup/LoadingSound");
+            if (authentication != null)
+            {
+                authentication.GetComponent<AudioSource>().Stop();
+            }
+            if (loadingscreen != null)
+            {
+                loadingscreenAudio = loadingscreen.GetComponent<AudioSource>();
+                loadingscreenAudio.Stop();
+            }
+            UnityWebRequest www = UnityWebRequest.Get("file://"+RandomMusicFile());
+            www.SendWebRequest();
+            while (!www.isDone)
+            {
+                yield return null;
+            }
+            AudioClip audioClip = WebRequestWWW.InternalCreateAudioClipUsingDH(www.downloadHandler, www.url, false, false, AudioType.UNKNOWN);
+            while (!www.isDone || audioClip.loadState == AudioDataLoadState.Loading)
+            {
+                yield return null;
+            }
+            if (audioClip != null)
+            {
+                if (authentication != null)
+                {
+                    authentication.GetComponent<AudioSource>().clip = audioClip;
+                    authentication.GetComponent<AudioSource>().Play();
+                }
+                if (loadingscreenAudio != null)
+                {
+                    loadingscreenAudio.clip = audioClip;
+                    loadingscreenAudio.Play();
+                }
+            }
+        }
     }
-
-
 }
 
 
